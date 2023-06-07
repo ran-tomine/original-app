1409976985cb795d26edd3669a880cee7758013c
ran02160 <ran02160ay@gmail.com>
2023-06-07 16:34:43 +0900

    投稿機能の実装

--- app/controllers/childcares_controller.rb
+++ app/controllers/childcares_controller.rb
@@ -1,17 +1,27 @@
 class ChildcaresController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :destroy]
+  
   def index
+    @childcare = Childcare.all
   end
 
   def new
+    @childcare = Childcare.new
   end
 
   def create
+    @childcare = Childcare.new(childcare_params)
+    if @childcare.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   private
 
-  def childcares_params
+  def childcare_params
     params.require(:childcare).permit(:title,:diary ,:image).merge(user_id: current_user.id)
   end
 end
