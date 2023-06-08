6f409b7a690d84c4138fafdc262c83185d42eb2b
ran02160 <ran02160ay@gmail.com>
2023-06-08 13:27:39 +0900

    newアクションデータベースへ保存

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,24 @@
 class EventsController < ApplicationController
 
 def new
-
+  @event = Event.new
 end
 
 def create
+  @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
   
+end
+
+
+private
+
+def event_params
+  params.require(:event).permit(:name,:location ,:datetime, :description, :image).merge(user_id: current_user.id)
 end
 
 end
