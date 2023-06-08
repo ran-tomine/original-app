94f86c672c4509fa15a91a8a401394829b2b42a4
ran02160 <ran02160ay@gmail.com>
2023-06-08 18:29:23 +0900

    フォロー人数の記載と一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :users do
     resource :relationships, only: [:create, :destroy]
+    get 'followings', to: 'followings#index'
   end
   
   resources :events do
