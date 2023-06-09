5954d49b4334fda5ccc6d1c1ec06f5af4b372a16
ran02160 <ran02160ay@gmail.com>
2023-06-09 14:24:12 +0900

    非同期処理の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_tweet
+
   def create
     like = current_user.likes.build(childcare_id: params[:childcare_id])
     like.save
-    redirect_to root_path
+    render 'childcares/destroy.js.erb'
   end
 
   def destroy
     like = Like.find_by(childcare_id: params[:childcare_id], user_id: current_user.id)
-    like.destroy
-    redirect_to root_path
+      like.destroy
+      render 'childcares/destroy.js.erb'
+  end
+
+  def set_tweet
+    @childcare = Childcare.find(params[:childcare_id])
   end
 end
