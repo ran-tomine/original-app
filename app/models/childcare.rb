aa28272c6854d87cf9b96445520e6cf4f4248ada
ran02160 <ran02160ay@gmail.com>
2023-06-09 13:42:42 +0900

    ユーザーがいいね済かどうかを判定

--- app/models/childcare.rb
+++ app/models/childcare.rb
@@ -8,4 +8,8 @@ class Childcare < ApplicationRecord
   has_one_attached :image
   has_many :childcare_comments, dependent: :destroy
   has_many :likes
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
