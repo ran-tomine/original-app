a815594631c560b598987caa28c21173a7f27df9
ran02160 <ran02160ay@gmail.com>
2023-06-13 10:39:48 +0900

    Googleマップ表示

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,7 @@ def new
 end
 
 def create
+  binding.pry
   @event = Event.new(event_params)
     if @event.save
       redirect_to root_path
@@ -43,7 +44,7 @@ end
 private
 
 def event_params
-  params.require(:event).permit(:name,:location ,:datetime, :description, :image).merge(user_id: current_user.id)
+  params.require(:event).permit(:name,:location ,:datetime, :description, :image, :address, :latitude, :longitude).merge(user_id: current_user.id)
 end
 
 end
