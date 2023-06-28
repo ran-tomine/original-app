92b4bcd920ab21de27724eb93181cf4820adb2ca
ran02160 <ran02160ay@gmail.com>
2023-06-28 14:11:31 +0900

    データベース修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,33 +21,34 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-
+  
     # 外部キー制約を一時的に無効化
     ActiveRecord::Base.transaction do
-    begin
-    ActiveRecord::Base.connection.execute("SET FOREIGN_KEY_CHECKS=0")
-
-    @user.childcares.destroy_all
-    @user.events.destroy_all
-    @user.childcare_comments.destroy_all
-    @user.event_comments.destroy_all
-    @user.rooms.destroy_all
-    @user.messages.destroy_all
-
-     # ユーザーの削除
-    @user.destroy
-
-    # 外部キー制約を再度有効化
-    ActiveRecord::Base.connection.execute("SET FOREIGN_KEY_CHECKS=1")
-
-    redirect_to root_path
-  rescue StandardError => e
-    # エラーハンドリング
-    # 例外が発生した場合にロールバックなどの処理を行うことができます
-    raise e
+      begin
+        ActiveRecord::Base.connection.execute("SET CONSTRAINTS ALL DEFERRED")
+  
+        @user.childcares.destroy_all
+        @user.events.destroy_all
+        @user.childcare_comments.destroy_all
+        @user.event_comments.destroy_all
+        @user.rooms.destroy_all
+        @user.messages.destroy_all
+  
+        # ユーザーの削除
+        @user.destroy
+  
+        # 外部キー制約を再度有効化
+        ActiveRecord::Base.connection.execute("SET CONSTRAINTS ALL IMMEDIATE")
+  
+        redirect_to root_path
+      rescue StandardError => e
+        # エラーハンドリング
+        # 例外が発生した場合にロールバックなどの処理を行うことができます
+        raise e
+      end
+    end
   end
-end
-end
+  
 
   private
 
