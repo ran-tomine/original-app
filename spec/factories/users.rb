5eaef3a6d6f0505fc482f13d00a1266be70e9b23
ran02160 <ran02160ay@gmail.com>
2023-06-29 13:27:34 +0900

    ユーザーテストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    profile               {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    profile               {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+
+    after(:build) do |user|
+      user.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,72 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it 'nicknameが空では登録できない' do
-      @user.nickname = ''  # nicknameの値を空にする
-      @user.valid?
-      expect(@user.errors.full_messages).to include "ニックネームを入力してください"
+    context '新規登録できるとき' do
+      it '画像とニックネームとEメールとプロフィール、パスワードと確認用のパスワードが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+
+      it '画像は空でも保存できること' do
+        @user.image = nil
+        expect(@user).to be_valid
+      end
     end
-    it 'emailが空では登録できない' do
-      @user.email = ''  # emailの値を空にする
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Eメールを入力してください"
+    context '新規登録できないとき' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''  # nicknameの値を空にする
+        @user.valid?
+        expect(@user.errors.full_messages).to include "ニックネームを入力してください"
+      end
+      it 'nicknameが6文字を超えると登録できない' do
+        @user.nickname = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('ニックネームは6文字以内で入力してください')
+      end
+
+      it 'profileが空では登録できない' do
+        @user.profile = ''  # profileの値を空にする
+        @user.valid?
+        expect(@user.errors.full_messages).to include "プロフィールを入力してください"
+      end
+      it 'profileが1000文字を超えると登録できない' do
+        @user.profile = 'a' * 1001
+        @user.valid?
+        expect(@user.errors.full_messages).to include('プロフィールは1000文字以内で入力してください')
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''  # emailの値を空にする
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Eメールを入力してください"
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+      end
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save(validate: false) 
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
+      end
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Eメールは不正な値です')
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '00000'
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
+      end
     end
   end
 end
