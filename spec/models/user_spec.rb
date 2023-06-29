fa9ee3deff00d91ce441597036653b3bbaaf5e0c
ran02160 <ran02160ay@gmail.com>
2023-06-29 12:01:43 +0900

    テストコード編集

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)  # Userのインスタンス生成
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      @user.nickname = ''  # nicknameの値を空にする
+      @user.valid?
+      expect(@user.errors.full_messages).to include "ニックネームを入力してください"
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''  # emailの値を空にする
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Eメールを入力してください"
+    end
+  end
+end
